e5bdb736dcc480f7206e22407490d1d10654a796
Lucas Claro <lucas-claro@hotmail.com>
2022-11-01 08:20:08 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-let city = 'araraquara'
-let resultCity = document.querySelector("#resultCity");
+const key = "##"
+const contryURL = "https://countryflagsapi.com/png/";
+const resultCity = document.querySelector("#resultCity");
+const searchButton = document.querySelector("#chooseCity");
+
+const cityElement = document.querySelector("#resultCity");
+const countryElement = document.querySelector(".conutryFlag");
+const weatherElement = document.querySelector(".weatherText");
+const imageElement = document.querySelector(".weatherImage");
+const temperatureElement = document.querySelector("#temperature");
+const humidityElement = document.querySelector("#humidity");
+const windElement = document.querySelector("#wind");
 
 
-resultCity.innerHTML = "São Paulo";
+const getWeatherData = async(resultCity) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${resultCity}&units=metric&appid=${key}&lang=pt_br`
 
+    const result = await fetch(apiWeatherURL);
+    const data = await result.json();
 
-const fetchCity = async (city) => {
-    const key = "80a4ad981dc134d36cc8ddea8b67a00a"
-    const link = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`);
-    let data = await link.json();
-    return data;
+    console.log(data);
 }
 
-console.log(data);
+
+const showWeatherData = (resultCity) => {
+
+    getWeatherData(resultCity);
+}
+
+
+function searchCity() {
+    const resultCity = document.querySelector("#chooseCity").value;
+
+
+}
